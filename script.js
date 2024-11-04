2ef3bc4bd57b3a71160d920b0dc4116eebbfcfa2
Sanni-Singh <aryansingh7371@gmail.com>
2024-11-04 13:52:32 +0530

    code

--- script.js
+++ script.js
@@ -4,35 +4,47 @@ const start = document.querySelector('.Start')
 const hour = document.querySelector('.hours')
 const minute = document.querySelector('.minutes')
 const second = document.querySelector('.seconds')
-let flag = false;
+let flag = true;
+let arr = [];
 let interval;
 function activateNumber(){
-    flag = true;
-     interval = setInterval(()=>{
-        second.innerText = Number(second.innerText) + 1;
-        
-        if(Number(second.innerText) === 60) {
-            minute.innerText = Number(minute.innerText) + 1;
-            second.innerText = 0
-        }
-        if(Number(minute.innerText) === 60) {
-            hour.innerText = Number(hour.innerText) + 1;
-            second.innerText = 0
-            minute.innerText = 0
-        }
-        if(figure === true) {
-            clearInterval(interval)
-        }
-    },1000)
-    
-
+     if(flag){
+        interval = setInterval(()=>{
+            second.innerText = Number(second.innerText) + 1;
+            
+            if(Number(second.innerText) === 60) {
+                minute.innerText = Number(minute.innerText) + 1;
+                second.innerText = 0
+            }
+            if(Number(minute.innerText) === 60) {
+                hour.innerText = Number(hour.innerText) + 1;
+                second.innerText = 0
+                minute.innerText = 0
+            }
+        },1000)
+        flag=false
+    }
 }
 
-start.addEventListener('click',activateNumber)
-reset.addEventListener('click',()=>{
+function stopInterval() {
+    clearInterval(interval)
+    flag=true
+}
+function resetTheInterval(){
+    clearInterval(interval);
+    arr.push(`${hour.innerText}-${minute.innerText}-${second.innerText}`)
     hour.innerText = "00";
     minute.innerText = "00";
     second.innerText = "00";
-    activateNumber()
-
+    flag = true;
+    
+    console.log(arr);
+}
+start.addEventListener('click',activateNumber)
+reset.addEventListener('click',()=>{
+    flag = false;
+    resetTheInterval();
 })
+pause.addEventListener('click',stopInterval);
+
+
